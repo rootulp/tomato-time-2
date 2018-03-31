097d9934f7b8a796c63dd060986fc15c021efed2
Rootul Patel <rootulp@palantir.com>
2018-03-31 17:18:10 +0100

    Create-react-app typescript

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Tomato Time 2 🍅</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>;
+      </div>
+    );
+  }
+}
+
+export default App;
